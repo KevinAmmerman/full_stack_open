09c0c3aebd5eb7fdc1cb2acc3cd860cc254fdb64
Kevin Ammerman <k.ammerman.git@pm.me>
2025-03-21 13:42:19 +0100

    Convert course parts to individual objects and refactor application

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,51 +1,57 @@
-const Header = (props) => {
-  return <h1>{props.header}</h1>;
+const Header = ({ header }) => {
+  console.log('header', header);
+  return <h1>{header}</h1>;
 };
 
-const Content = (props) => {
+const Content = ({ part1, part2, part3 }) => {
+  console.log('Content', part1, part2, part3);
   return (
     <div>
-      <Part part={props.parts[0].name} amount={props.parts[0].amount} />
-      <Part part={props.parts[1].name} amount={props.parts[1].amount} />
-      <Part part={props.parts[2].name} amount={props.parts[2].amount} />
+      <Part part={part1.name} exercises={part1.exercises} />
+      <Part part={part2.name} exercises={part2.exercises} />
+      <Part part={part3.name} exercises={part3.exercises} />
     </div>
   );
 };
 
-const Part = (props) => {
+const Part = ({ part, exercises }) => {
+  console.log('Part', part, exercises);
   return (
     <p>
-      {props.part}: {props.amount}
+      {part}: {exercises}
     </p>
   );
 };
 
-const Total = (props) => {
-  return <p>Number of exercises: {props.total}</p>;
+const Total = ({ part1, part2, part3 }) => {
+  console.log('Total', part1, part2, part3);
+  return (
+    <p>
+      Number of exercises: {part1.exercises + part2.exercises + part3.exercises}
+    </p>
+  );
 };
 
 const App = () => {
   const course = 'Half Stack application development';
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      amount: 10,
-    },
-    {
-      name: 'Using props to pass data',
-      amount: 7,
-    },
-    {
-      name: 'State of a component',
-      amount: 14,
-    },
-  ];
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10,
+  };
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7,
+  };
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14,
+  };
 
   return (
     <>
       <Header header={course} />
-      <Content parts={parts} />
-      <Total total={parts[0].amount + parts[1].amount + parts[2].amount} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3} />
     </>
   );
 };
