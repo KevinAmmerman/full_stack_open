c179f66306c154b235b04e870f6887396ef1c381
Kevin Ammerman <k.ammerman.git@pm.me>
2025-03-21 13:52:19 +0100

    Consolidate course parts into a single array and refactor application

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -3,13 +3,13 @@ const Header = ({ header }) => {
   return <h1>{header}</h1>;
 };
 
-const Content = ({ part1, part2, part3 }) => {
-  console.log('Content', part1, part2, part3);
+const Content = ({ parts }) => {
+  console.log('Content', parts);
   return (
     <div>
-      <Part part={part1.name} exercises={part1.exercises} />
-      <Part part={part2.name} exercises={part2.exercises} />
-      <Part part={part3.name} exercises={part3.exercises} />
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
     </div>
   );
 };
@@ -23,35 +23,38 @@ const Part = ({ part, exercises }) => {
   );
 };
 
-const Total = ({ part1, part2, part3 }) => {
-  console.log('Total', part1, part2, part3);
+const Total = ({ parts }) => {
+  console.log('Total', parts);
   return (
     <p>
-      Number of exercises: {part1.exercises + part2.exercises + part3.exercises}
+      Number of exercises:{' '}
+      {parts[0].exercises + parts[1].exercises + parts[2].exercises}
     </p>
   );
 };
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <>
       <Header header={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </>
   );
 };
