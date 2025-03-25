253885496390838bdfad63a2b0e4a7efc93345b5
Kevin Ammerman <k.ammerman.git@pm.me>
2025-03-25 12:00:07 +0100

    Enhance statistics with average calculations and percentage of positive feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,10 +7,42 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
-  const handleGoodClick = () => setGood(good + 1);
-  const handleNeutralClick = () => setNeutral(neutral + 1);
-  const handleBadClick = () => setBad(bad + 1);
+  const calcPositive = (good, total) => {
+    const positiveResult = (good / total) * 100;
+    const roundedResult = Math.ceil(positiveResult * 100) / 100;
+    setPositive(roundedResult);
+  };
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    setTotal(total + 1);
+    const updatedTotal = total + 1;
+    const updatedGood = good + 1;
+    const averageResult = (updatedGood - bad) / updatedTotal;
+    const roundedResult = Math.ceil(averageResult * 100) / 100;
+    setAverage(roundedResult);
+    calcPositive(updatedGood, updatedTotal);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    setTotal(total + 1);
+    const updatedTotal = total + 1;
+    calcPositive(good, updatedTotal);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    setTotal(total + 1);
+    const updatedTotal = total + 1;
+    const updatedBad = bad + 1;
+    const averageResult = (good - updatedBad) / updatedTotal;
+    const roundedResult = Math.ceil(averageResult * 100) / 100;
+    setAverage(roundedResult);
+    calcPositive(good, updatedTotal);
+  };
 
   return (
     <>
@@ -22,6 +54,9 @@ const App = () => {
       <p>good: {good}</p>
       <p>neutral: {neutral}</p>
       <p>bad: {bad}</p>
+      <p>all: {total}</p>
+      <p>average: {average}</p>
+      <p>positive: {positive}%</p>
     </>
   );
 };
