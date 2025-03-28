e4f903cb65466d4944db916cc941fdea7acf3ecb
Kevin Ammerman <k.ammerman.git@pm.me>
2025-03-28 11:38:54 +0100

    Create phonebook application with dynamic person list, controlled form input, and functionality to add new persons

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const addNewPerson = (event) => {
+    event.preventDefault();
+    const newObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(newObject));
+    setNewName('');
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type='submit' onClick={addNewPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
