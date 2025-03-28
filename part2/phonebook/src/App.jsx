f31003775694e770cc041fd19d66025ab01427af
Kevin Ammerman <k.ammerman.git@pm.me>
2025-03-28 14:49:35 +0100

    Implement filter functionality with case-insensitive name search and dynamic filtering of contact list

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,32 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '01848654565' },
+  const [persons, setOriginalPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ]);
+  const [filteredPersons, setFilteredPersons] = useState([...persons]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (event) => setNewName(event.target.value);
 
   const handleNumberChange = (event) => setNewNumber(event.target.value);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+    const filterInside = event.target.value.toLowerCase();
+    const filtered = persons.filter((person) => {
+      const [firstName, lastName] = person.name.toLowerCase().split(' ');
+      return (
+        firstName.startsWith(filterInside) || lastName.startsWith(filterInside)
+      );
+    });
+    setFilteredPersons(filtered);
+  };
 
   const addNewPerson = (event) => {
     event.preventDefault();
@@ -20,7 +37,7 @@ const App = () => {
         name: newName,
         number: newNumber,
       };
-      setPersons(persons.concat(newObject));
+      setOriginalPersons(persons.concat(newObject));
       setNewName('');
       setNewNumber('');
     }
@@ -37,6 +54,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input value={filter} onChange={handleFilterChange} />
+      </div>
       <form>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -52,7 +73,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
+        {filteredPersons.map((person) => (
           <li key={person.name}>
             {person.name} {person.number}
           </li>
