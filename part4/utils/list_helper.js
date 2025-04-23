6e8764098c1ba6290c4260414ff2360c34d01bac
Kevin Ammerman <k.ammerman.git@pm.me>
2025-04-23 22:19:47 +0200

    Add helper functions and unit tests for blog list
    Implement a dummy function returning a constant value and set up test
    configuration. Add totalLikes function to calculate the sum of likes
    across blog posts, with corresponding unit tests in a describe block.
    Reference Exercises 4.3 and 4.4.

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+const dummy = (blogs) => {
+  return 1
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, blog) => (acc += blog.likes), 0)
+}
+
+module.exports = {
+  dummy,
+  totalLikes,
+}
