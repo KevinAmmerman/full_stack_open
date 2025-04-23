97470dfbf264dc68bb0c8ab74f2d94e456f8a65f
Kevin Ammerman <k.ammerman.git@pm.me>
2025-04-23 23:24:18 +0200

    Add favoriteBlog function with unit tests
    Implement favoriteBlog function to return the blog with the most likes
    from a list of blogs. Add corresponding unit tests in a dedicated
    describe block using deepStrictEqual for object comparison. Reference
    Exercise 4.5.

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -1,7 +1,59 @@
-/* eslint-disable no-unused-vars */
 const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+
+const blogs = [
+  {
+    _id: '5a422a851b54a676234d17f7',
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: '5a422b3a1b54a676234d17f9',
+    title: 'Canonical string reduction',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: '5a422b891b54a676234d17fa',
+    title: 'First class tests',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: '5a422ba71b54a676234d17fb',
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: '5a422bc61b54a676234d17fc',
+    title: 'Type wars',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+    likes: 2,
+    __v: 0,
+  },
+]
+
+const blogsTwo = []
 
 test('dummy returns one', () => {
   const blogs = []
@@ -11,59 +63,37 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const blogs = [
-    {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: '5a422b891b54a676234d17fa',
-      title: 'First class tests',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: '5a422ba71b54a676234d17fb',
-      title: 'TDD harms architecture',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: '5a422bc61b54a676234d17fc',
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2,
-      __v: 0,
-    },
-  ]
-
-  test('when list has multiple blogs, equals the likes of that', () => {
+  test('When the list contains multiple blogs, it sums the likes of all.', () => {
     const result = listHelper.totalLikes(blogs)
     assert.strictEqual(result, 36)
+  })
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(blogsTwo)
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const singleBlogArray = [blogs[0]]
+    const result = listHelper.totalLikes(singleBlogArray)
+    assert.strictEqual(result, 7)
+  })
+})
+
+describe('favorite blog', () => {
+  test('returns the blog with the most likes from a list of blogs', () => {
+    const result = listHelper.favoriteBlog(blogs)
+    assert.deepStrictEqual(result, blogs[2])
+  })
+
+  test('of empty list equals undefined', () => {
+    const result = listHelper.favoriteBlog([])
+    assert.deepStrictEqual(result, undefined)
+  })
+
+  test('when list has only one blog, returns that one', () => {
+    const singleBlogArray = [blogs[0]]
+    const result = listHelper.favoriteBlog(singleBlogArray)
+    assert.deepStrictEqual(result, blogs[0])
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -7,7 +7,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, blog) => (acc += blog.likes), 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const blogsCopy = [...blogs]
+  return blogsCopy.sort((a, b) => b.likes - a.likes)[0]
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
