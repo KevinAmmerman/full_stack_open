7d56de163808370326d07d7298a7ba59c3a25dd5
Kevin Ammerman <k.ammerman.git@pm.me>
2025-05-02 20:56:16 +0200

    Add mostLikes function and refactor tests for invalid inputs
    Implement mostLikes function to return the author with the highest total
    likes along with the count. Refactor existing tests to handle various
    invalid inputs in a unified test case for better coverage, less code,
    and improved readability. Reference Exercise 4.7.

--- part4/tests/list_helper.test.js
+++ part4/tests/list_helper.test.js
@@ -92,30 +92,18 @@ describe('total likes', () => {
     assert.strictEqual(result, 0)
   })
 
-  test('Return 0 if the list is empty.', () => {
-    const result = listHelper.totalLikes([])
-    assert.strictEqual(result, 0)
-  })
-
   test('Returns the likes of a single blog if the list contains only one blog.', () => {
     const singleBlogArray = [blogs[0]]
     const result = listHelper.totalLikes(singleBlogArray)
     assert.strictEqual(result, 7)
   })
 
-  test('Returns 0 if the input is not an array.', () => {
-    const result = listHelper.totalLikes({})
-    assert.strictEqual(result, 0)
-  })
-
-  test('Returns 0 if the input is null.', () => {
-    const result = listHelper.totalLikes(null)
-    assert.strictEqual(result, 0)
-  })
-
-  test('Returns 0 if the input is undefined.', () => {
-    const result = listHelper.totalLikes(undefined)
-    assert.strictEqual(result, 0)
+  test('Returns 0 for various invalid inputs.', () => {
+    const invalidInputs = [undefined, null, [], {}, 123, 'No array', true]
+    invalidInputs.forEach((input) => {
+      const result = listHelper.totalLikes(input)
+      assert.strictEqual(result, 0)
+    })
   })
 })
 
@@ -123,11 +111,6 @@ describe('favorite blog', () => {
   test('Returns the blog with the most likes from a list of blogs.', () => {
     const result = listHelper.favoriteBlog(blogs)
     assert.deepStrictEqual(result, blogs[2])
-  })
-
-  test('Return 0 if the list is empty.', () => {
-    const result = listHelper.favoriteBlog([])
-    assert.deepStrictEqual(result, 0)
   })
 
   test('When the list contains only one blog, it returns that blog.', () => {
@@ -141,19 +124,12 @@ describe('favorite blog', () => {
     assert.deepStrictEqual(result, blogs[2])
   })
 
-  test('Returns 0 if the input is not an array.', () => {
-    const result = listHelper.favoriteBlog({})
-    assert.deepStrictEqual(result, 0)
-  })
-
-  test('Returns 0 if the input is null.', () => {
-    const result = listHelper.favoriteBlog(null)
-    assert.deepStrictEqual(result, 0)
-  })
-
-  test('Returns 0 if the input is undefined.', () => {
-    const result = listHelper.favoriteBlog(undefined)
-    assert.deepStrictEqual(result, 0)
+  test('Returns 0 for various invalid inputs.', () => {
+    const invalidInputs = [undefined, null, [], {}, 123, 'No array', true]
+    invalidInputs.forEach((input) => {
+      const result = listHelper.favoriteBlog(input)
+      assert.strictEqual(result, 0)
+    })
   })
 })
 
@@ -166,24 +142,12 @@ describe('Most blogs', () => {
     })
   })
 
-  test('Returns 0 if the input is not an array.', () => {
-    const result = listHelper.mostBlogs({})
-    assert.deepStrictEqual(result, 0)
-  })
-
-  test('Returns 0 if the input is null.', () => {
-    const result = listHelper.mostBlogs(null)
-    assert.deepStrictEqual(result, 0)
-  })
-
-  test('Returns 0 if the input is undefined.', () => {
-    const result = listHelper.mostBlogs(undefined)
-    assert.deepStrictEqual(result, 0)
-  })
-
-  test('Return 0 if the list is empty.', () => {
-    const result = listHelper.mostBlogs([])
-    assert.deepStrictEqual(result, 0)
+  test('Returns { author: undefined, likes: 0 } for various invalid inputs', () => {
+    const invalidInputs = [undefined, null, 'No string', 1234, [], {}, true]
+    invalidInputs.forEach((input) => {
+      const result = listHelper.mostBlogs(input)
+      assert.deepStrictEqual(result, { author: undefined, blogs: 0 })
+    })
   })
 
   test('When the list contains only one blog, it returns that blogs author.', () => {
@@ -193,5 +157,61 @@ describe('Most blogs', () => {
       author: 'Michael Chan',
       blogs: 1,
     })
+  })
+})
+
+describe('max likes', () => {
+  test('Returns the author with the most likes from a list of blogs.', () => {
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, {
+      author: 'Edsger W. Dijkstra',
+      likes: 17,
+    })
+  })
+
+  test('Returns { author: undefined, likes: 0 } for various invalid inputs', () => {
+    const invalidInputs = [undefined, null, 'No string', 1234, [], {}, true]
+    invalidInputs.forEach((input) => {
+      const result = listHelper.mostLikes(input)
+      assert.deepStrictEqual(result, { author: undefined, likes: 0 })
+    })
+  })
+
+  test('Ignores blogs where likes have a non-numeric value.', () => {
+    const blogs = [
+      {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+        __v: 0,
+      },
+      {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 'not a number',
+        __v: 0,
+      },
+    ]
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, { author: 'Robert C. Martin', likes: 2 })
+  })
+
+  test('If the list contains only one blog, returns it.', () => {
+    const blogs = [
+      {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+        __v: 0,
+      },
+    ]
+    const result = listHelper.mostLikes(blogs)
+    assert.deepStrictEqual(result, { author: 'Robert C. Martin', likes: 2 })
   })
 })
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -22,10 +22,21 @@ const favoriteBlog = (blogs) => {
 }
 
 const mostBlogs = (blogs) => {
-  if (!Array.isArray(blogs) || blogs.length === 0) return 0
+  if (!Array.isArray(blogs) || blogs.length === 0) return { author: undefined, blogs: 0 }
   const authorBlog = _.maxBy(blogs, 'author')
   const count = _.countBy(blogs, (blog) => blog.author === authorBlog.author)
   return { author: authorBlog.author, blogs: count.true }
+}
+
+const mostLikes = (blogs) => {
+  if (!Array.isArray(blogs) || blogs.length === 0) return { author: undefined, likes: 0 }
+  const groupedBlogs = _.groupBy(blogs, 'author')
+  const authorTotals = _.map(groupedBlogs, (blogs, authorName) => {
+    const totalLikes = _.sumBy(blogs, (blog) => (typeof blog.likes === 'number' ? blog.likes : 0))
+    return { author: authorName, likes: totalLikes }
+  })
+  const authorWithMostLikes = _.maxBy(authorTotals, 'likes')
+  return authorWithMostLikes
 }
 
 module.exports = {
@@ -33,4 +44,5 @@ module.exports = {
   totalLikes,
   favoriteBlog,
   mostBlogs,
+  mostLikes,
 }
