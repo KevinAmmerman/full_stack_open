f314a0fa4b3b97877dbf39058b6de04ecd1c1800
Kevin Ammerman <k.ammerman.git@pm.me>
2025-05-07 20:15:16 +0200

    Add test for creating blog post and refactor to async/await
    Implement test to verify that an HTTP POST request to /api/blogs
    successfully creates a new blog post, checking the increase in total
    blogs and content accuracy. Refactor the POST operation to use
    async/await syntax instead of promises. Reference Exercise 4.10.

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -14,25 +14,52 @@ beforeEach(async () => {
 })
 
 describe('read blogs from server', () => {
-  test.only('notes are returned as json', async () => {
+  test('notes are returned as json', async () => {
     await api
       .get('/api/blogs')
       .expect(200)
       .expect('Content-Type', /application\/json/)
   })
 
-  test.only('all blogs are returned', async () => {
+  test('all blogs are returned', async () => {
     const blogs = await helper.blogsInDb()
 
     assert.strictEqual(blogs.length, helper.initialBlogs.length)
   })
 
-  test.only('blog post unique identifier is named id, not _id', async () => {
+  test('blog post unique identifier is named id, not _id', async () => {
     const response = await api.get('/api/blogs')
-    const isKeyId = response.body.some((blog) => {
+    const isKeyId = response.body.every((blog) => {
       return Object.keys(blog).find((key) => key === 'id')
     })
     assert(isKeyId, true)
+  })
+})
+
+describe('send new blog to server', () => {
+  test('add new blog to database', async () => {
+    const newBlogObject = {
+      title: 'React patterns',
+      author: 'Kevin Ammerman',
+      url: 'https://reactpatterns.com/',
+      likes: 70,
+    }
+    const response = await api
+      .post('/api/blogs')
+      .send(newBlogObject)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const newBlogId = response.body.id
+    const newBlogInDb = await Blog.findById(newBlogId)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    assert.ok(newBlogId, 'new created blog should be found on data base')
+    assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length + 1)
+    assert.strictEqual(newBlogInDb.title, newBlogObject.title)
+    assert.strictEqual(newBlogInDb.author, newBlogObject.author)
+    assert.strictEqual(newBlogInDb.url, newBlogObject.url)
+    assert.strictEqual(newBlogInDb.likes, newBlogObject.likes)
   })
 })
 
