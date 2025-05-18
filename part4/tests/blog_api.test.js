0a5a8e31c012903ac634a54a5a333eb52553376c
Kevin Ammerman <k.ammerman.git@pm.me>
2025-05-18 14:52:33 +0200

    Add user creation functionality with tests
    Implement user creation via POST to /api/users with username, password
    (encrypted using bcrypt), and name. Add endpoint to list all users.
    Include comprehensive tests for user creation scenarios. Reference
    Exercise 4.15.

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -1,10 +1,12 @@
 const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
 const app = require('../app')
 const assert = require('node:assert')
 const helper = require('./test_helper')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const api = supertest(app)
 
@@ -176,7 +178,7 @@ describe('DELETE /api/blogs', () => {
 })
 
 describe('PUT /api/blogs', () => {
-  test.only('successfully updates the blog likes', async () => {
+  test('successfully updates the blog likes', async () => {
     const blogIdToChange = '5a422a851b54a676234d17f7'
     const blogAtStart = await Blog.findById(blogIdToChange)
 
@@ -187,7 +189,7 @@ describe('PUT /api/blogs', () => {
     assert.strictEqual(blogAtEnd.body.likes, 20, 'Likes should be updated to 20 in the database')
   })
 
-  test.only('returns status 404 if id is ', async () => {
+  test('returns status 404 if id is ', async () => {
     const testBlog = new Blog({
       title: 'Angular patterns',
       author: 'Hans Zimmer',
@@ -197,7 +199,7 @@ describe('PUT /api/blogs', () => {
     await api.put(`/api/blogs/${testBlog._id}`).send(testBlog).expect(404)
   })
 
-  test.only('returns status 500 if id is invalid', async () => {
+  test('returns status 500 if id is invalid', async () => {
     const invalidId = 'asd7a8f7s89d8f7sdfd'
     const testBlog = new Blog({
       title: 'Angular patterns',
@@ -206,6 +208,60 @@ describe('PUT /api/blogs', () => {
     })
 
     await api.put(`/api/blogs/${invalidId}`).send(testBlog).expect(500)
+  })
+})
+
+describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test.only('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map((u) => u.username)
+    assert(usernames.includes(newUser.username))
+  })
+
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    assert(result.body.error.includes('expected `username` to be unique'))
+
+    assert.strictEqual(usersAtEnd.length, usersAtStart.length)
   })
 })
 
