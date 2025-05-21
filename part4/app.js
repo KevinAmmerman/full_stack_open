9a860d49c9a26d46a8cb3cb78ed2c551f49df8cb
Kevin Ammerman <k.ammerman.git@pm.me>
2025-05-21 21:46:05 +0200

    Refactor token extraction into middleware
    Create a tokenExtractor middleware to retrieve the token from the
    Authorization header and assign it to the request object. Register the
    middleware before all routes to enable token access in route handlers.
    Reference Exercise 4.20.

--- part4/app.js
+++ part4/app.js
@@ -19,6 +19,7 @@ mongoose
   })
 
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,13 +3,6 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-}
-
 blogsRouter.get('/', async (request, response, next) => {
   try {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1 })
@@ -21,8 +14,7 @@ blogsRouter.get('/', async (request, response, next) => {
 
 blogsRouter.post('/', async (request, response, next) => {
   try {
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
-    console.log('DECODED TOKEN #####', decodedToken)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
       return response.status(401).json({ error: 'token invalid' })
     }
