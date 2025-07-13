07d3b375353b11ca09c851190ffe884deafdf963
Kevin Ammerman <k.ammerman.git@pm.me>
2025-07-13 13:40:51 +0200

    feat(redux): integrate Redux Toolkit for store management
    -                             Install Redux Toolkit and configure store using configureStore in store.js
    -                             Refactor filter reducer and action creators with createSlice
    -                             Related to Exercise 6.10

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,15 +1,9 @@
 import ReactDOM from 'react-dom/client'
-import { legacy_createStore as createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecdoteReducer from './reducers/anecdoteReducer'
-import filterReducer from './reducers/filterReducer'
+import store from './store'
 
-const reducers = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer,
-})
-const store = createStore(reducers)
+console.log(store.getState())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
