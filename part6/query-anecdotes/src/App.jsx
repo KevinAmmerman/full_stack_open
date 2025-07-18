fa9f03ddfa0fa010ec7a55fd12c3cbbc830094fe
Kevin Ammerman <k.ammerman.git@pm.me>
2025-07-18 21:57:57 +0200

    feat(react-query): implement anecdote retrieval with React Query
    -                          Fetch anecdotes from the server using React Query
    -                          Handle server communication errors by displaying an error page
    -                          Create a service for managing API requests
    -                          Related to Exercise 6.20

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,18 +1,24 @@
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAll } from './services/anecdotes'
 
 const App = () => {
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  const queryClient = useQueryClient()
+
+  const query = useQuery({ queryKey: ['anecdotes'], queryFn: getAll })
+
+  if (query.isLoading) {
+    return <div>loading data...</div>
+  } else if (query.isError) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = query.data
 
   return (
     <div>
