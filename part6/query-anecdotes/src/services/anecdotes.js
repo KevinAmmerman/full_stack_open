77c635d02612c19caad92ea2196ad258b7df4cb3
Kevin Ammerman <k.ammerman.git@pm.me>
2025-07-18 22:59:44 +0200

    feat(react-query): implement adding new anecdotes to server
    -                                   Add functionality to create new anecdotes using React Query
    -                                   Ensure new anecdotes are rendered by default in the application
    -                                   Related to Exercise 6.21

--- part6/query-anecdotes/src/services/anecdotes.js
+++ part6/query-anecdotes/src/services/anecdotes.js
@@ -6,3 +6,8 @@ export const getAll = async () => {
   const response = await axios.get(baseUrl)
   return response.data
 }
+
+export const createAnecdote = async (object) => {
+  const response = await axios.post(baseUrl, object)
+  return response.data
+}
