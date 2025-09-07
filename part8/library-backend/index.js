c8266271452b154a0825aaf4b03da82934b6ca04
Kevin Ammerman <k.ammerman.git@pm.me>
2025-09-07 14:51:43 +0200

    ✨ feat(books): implement author filtering for allBooks query
    - add author argument to allBooks query to allow filtering by author name
    - update resolver to filter books based on provided author argument
    
    Related to exercise 8.4

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -107,7 +107,7 @@ const typeDefs = `
     type Query {
         bookCount: Int!
         authorCount: Int!
-        allBooks: [Book!]!
+        allBooks(author: String!): [Book!]!
         allAuthors: [AuthorBookCount!]!
     }
 `
@@ -116,7 +116,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books
+      return books.filter((book) => book.author === args.author)
+    },
     allAuthors: () => {
       return authors.map((author) => ({
         name: author.name,
